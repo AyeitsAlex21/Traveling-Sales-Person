f336700cfa3bacee19ff9368cdc807618f344379
Niklaas Cotta <nikcotta@gmail.com>
2021-10-10 13:26:37 -0700

    example output for API

--- google-maps-api.py
+++ google-maps-api.py
@@ -3,18 +3,30 @@ Source Code for Google Maps API, CIS422 FA21
 Author(s): Niklaas Cotta
 Last Edited: 10/09/21
 Sources:
-  List of APIs: https://console.cloud.google.com/google/maps-apis/new?project=celestial-brand-308820
+  List of APIs: https://developers.google.com/maps/documentation
   Distance Matrix Documentation: https://developers.google.com/maps/documentation/distance-matrix/start
 """
 import requests
 
-API_KEY = "API_KEY"  # hide this
-url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key="
-url += API_KEY
 
-payload={}
-headers = {}
+def get_distance():
+    API_KEY = ""  # hide this
+    origin = "Washington%2C%20DC"
+    dest = "New%20York%20City%2C%20NY"
 
-response = requests.request("GET", url, headers=headers, data=payload)
+    url = f"https://maps.googleapis.com/maps/api/distancematrix/json?" \
+          f"origins={origin}&destinations={dest}" \
+          f"&units=imperial&key={API_KEY}"
 
-print(response.text)
+    response = requests.request("GET", url, headers={}, data={})
+    # print(response.text)
+    output = response.json()
+
+    print("Place of Origin:  ", output["destination_addresses"][0])
+    print("Destination:      ", output["origin_addresses"][0])
+    print("Distance between: ", output["rows"][0]["elements"][0]["distance"]["text"])
+    print("Time taken:       ", output["rows"][0]["elements"][0]["duration"]["text"])
+
+
+if __name__ == '__main__':
+    get_distance()
