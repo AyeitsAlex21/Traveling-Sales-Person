4eace744c2266aa5a87ef155a4be6f8c3a3e4730
Niklaas Cotta <nikcotta@gmail.com>
2021-10-14 19:00:34 -0700

    created example output for API

--- google-maps-api.py
+++ google-maps-api.py
@@ -9,24 +9,37 @@ Sources:
 import requests
 
 
-def get_distance():
-    API_KEY = ""  # hide this
-    origin = "Washington%2C%20DC"
-    dest = "New%20York%20City%2C%20NY"
+def get_distance(origins, destinations):
+    """
+    :param origins: list of addresses (strings)
+    :param destinations: list of addresses (strings)
+    :return: list of distances (ints)
+    """
+    API_KEY = "AIzaSyAiEaWFHFSoyuO3AmXwP4SIBLTH4a6pzts"  # need to hide this
+    i = 1
+    distances = []
 
-    url = f"https://maps.googleapis.com/maps/api/distancematrix/json?" \
-          f"origins={origin}&destinations={dest}" \
-          f"&units=imperial&key={API_KEY}"
+    # base url
+    for origin, destination in zip(origins, destinations):
+        url = f"https://maps.googleapis.com/maps/api/distancematrix/json?" \
+              f"origins={origin}&destinations={destination}" \
+              f"&units=imperial&key={API_KEY}"
 
-    response = requests.request("GET", url, headers={}, data={})
-    # print(response.text)
-    output = response.json()
+        # get request
+        response = requests.request("GET", url, headers={}, data={})
+        # print(response.text)
+        output = response.json()
+        distances.append(output["rows"][0]["elements"][0]["distance"]["text"])
 
-    print("Place of Origin:  ", output["destination_addresses"][0])
-    print("Destination:      ", output["origin_addresses"][0])
-    print("Distance between: ", output["rows"][0]["elements"][0]["distance"]["text"])
-    print("Time taken:       ", output["rows"][0]["elements"][0]["duration"]["text"])
+        # formatted output (temp)
+        print("Distance #", i)
+        print("Place of Origin:  ", output["destination_addresses"][0])
+        print("Destination:      ", output["origin_addresses"][0])
+        print("Distance between: ", output["rows"][0]["elements"][0]["distance"]["text"])
+        print("Time taken:       ", output["rows"][0]["elements"][0]["duration"]["text"])
+        print("\n")
 
 
 if __name__ == '__main__':
-    get_distance()
+    # Example
+    get_distance(["6128 Flag Point Drive, Ooltewah"], ["1710 E 15th Ave, Eugene"])
