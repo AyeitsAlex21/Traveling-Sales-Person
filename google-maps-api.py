025fec59572b0886a286e77de9e2242f9174e32d
Niklaas Cotta <nikcotta@gmail.com>
2021-10-18 07:54:36 -0700

    added documentation and basic error handling

--- google-maps-api.py
+++ google-maps-api.py
@@ -12,18 +12,36 @@ import re
 
 def get_distance(origin, destination):
     """
+    This function takes a source string and a destination string in the form of an address.
+    Address may be in following forms:
+        1710 E 15th Ave, OR
+        6513 Flag Way Dr, Boise, Idaho
+        Seattle, Washington
+        San Francisco, CA
+    Function then requests distance between source and destination from google maps API.
+    If successful request, gets the distance from the json, and converts the distance from a string to integer
+    On failure returns -1. On success returns distance between the two places.
     :param origin: str
     :param destination: str
     :return: int (distance between the two)
     """
-    API_KEY = ""  # need to hide this
+
+    # get API key
+    fp = open("api-key.txt", "r")
+    API_KEY = fp.read()
+    fp.close()
 
     # base url
     url = f"https://maps.googleapis.com/maps/api/distancematrix/json?" \
           f"origins={origin}&destinations={destination}" \
           f"&units=imperial&key={API_KEY}"
 
-    response = requests.request("GET", url, headers={}, data={}).json()  # query response from google maps
+    response = requests.request("GET", url, headers={}, data={})  # query response from google maps
+    if response.status_code != 200:
+        print("Could not get distance from API")
+        return -1
+
+    response = response.json()
     distancestr = response["rows"][0]["elements"][0]["distance"]["text"]  # isolate .json element
 
     # this just turns the string into integer form
@@ -35,7 +53,12 @@ def get_distance(origin, destination):
 
 def genMatrix(addressList):
     """
-    :param addressList: a list of addresses, first is origin
+    This function takes a list of addresses (strings) and generates a complete graph of distances between addresses.
+    This graph is in the form of a matrix where each index corresponds to an address, in the order of addressList.
+    After initially populating the matrix with 0s, the graph then calls get_distance() between each pair of addresses.
+    The graph is undirected, so the matrix will have symmetry.
+
+    :param addressList: list of str
     :return: tuple containing list of list of distances (matrix) and list of addresses (strings)
     """
     matrix = []
@@ -48,19 +71,20 @@ def genMatrix(addressList):
             matrix[j].append(0)
 
     for j in range(n):
-        for i in range(n):
+        for i in range(j, n):  # we consider distance same in both directions, reduces request waiting time
             if i != j:  # distance from x to x is 0
                 distance = get_distance(addressList[i], addressList[j])
 
                 matrix[i][j] = distance
-                matrix[j][i] = distance  # undirected graph
+                matrix[j][i] = distance  # distances are same
 
-        # print(matrix[j])
-    # print(addressList)
-
-    return addressList, matrix  # returns tuple containing address list and corresponding matrix
+    return matrix, addressList  # returns tuple containing address list and corresponding matrix
 
 
 if __name__ == '__main__':
     # Example
-    addresses, addressMat = genMatrix(["NYC, NY", "1710 E 15th Ave, Eugene,OR", "Cocoa Beach,FL", "Seattle, Washington"])
+    newAddresses = ["NYC, NY", "1710 E 15th Ave, Eugene,OR", "Cocoa Beach,FL", "Seattle, Washington"]
+    newMatrix, addresses = genMatrix(newAddresses)
+
+    for address in newMatrix:
+        print(address)
